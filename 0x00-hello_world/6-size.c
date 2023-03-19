0c7b60970279975e8ca748d39a29365820c65d4b
Aya <aya_shamseldin@yahoo.com>
2023-03-19 14:12:53 -0700

    c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	long int a = 8;
 
-printf("Size of a char: %ld bytes(s)\n", sizeof(char));
-printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)", sizeof(int));
 printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld bytes(s)\n", a);
 printf("Size of a float: %ld bytes(s)\n", sizeof(float));
