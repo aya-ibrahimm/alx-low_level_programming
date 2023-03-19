5450a2ee59a93d85a8619132236254a5ba0c10ca
Aya <aya_shamseldin@yahoo.com>
2023-03-19 13:13:43 -0700

    c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stio.h>
+
+/**
+ * main- first comment in c
+ *
+ * Description: a c program
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
