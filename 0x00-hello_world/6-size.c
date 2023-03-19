df60e4b91d00ab888ade48a85214def4a0de9720
Aya <aya_shamseldin@yahoo.com>
2023-03-19 13:57:26 -0700

    c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 printf("Size of a char: %lu bytes(s)/n", sizeof(char));
 printf("Size of an int: %lu bytes(s)/n", sizeof(int));
 printf("Size of a long int: %lu bytes(s)/n", sizeof(long int));
-printf("Size of a long long int: %llu bytes(s)/n", sizeof(long long int));
+printf("Size of a long long int: %d bytes(s)/n", sizeof(long long int) * 2);
 printf("Size of a float: %lu bytes(s)/n", sizeof(float));
 return (0);
