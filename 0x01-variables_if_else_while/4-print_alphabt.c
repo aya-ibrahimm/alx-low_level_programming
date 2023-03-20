4808a351896813e8272de134bf464851d3a09f33
Aya <aya_shamseldin@yahoo.com>
2023-03-20 14:42:50 -0700

    c programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - c program
+ *
+ *Return: return 0 (success)
+ */
+int main(void)
+{
+	char *s = "abcdfghijklmnoprstuvwxyz1";
+	int i = 0;
+	while (s[i] != '1')
+	{
+	putchar(s[i]);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
