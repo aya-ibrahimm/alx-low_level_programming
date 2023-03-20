f63ed117ee23edb43bf3305660f4e8e7c3a575a8
Aya <aya_shamseldin@yahoo.com>
2023-03-20 14:53:22 -0700

    c programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - c program
+ *
+ *Return: return 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; ++i)
+	putchar(i + '0');
+putchar('\n');
+return (0);
+}
