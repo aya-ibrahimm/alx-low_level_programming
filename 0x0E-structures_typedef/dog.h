6c9abedc008f741974ce168070258aae2bfbcdba
Aya <aya_shamseldin@yahoo.com>
2023-04-18 15:27:05 -0700

    struct project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef creating_struct_dog_H
+#define creating_struct_dog_H
+
+/**
+ * struct - creating a struct for dog
+ * @name: write the dog name
+ * @age: define the dog's age
+ * @owner: see who is owner of the dog
+ *
+ * Description: struct for dogs
+ *
+ * Return: always return 0
+ */
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+#endif /* creating_struct_dog_H */
