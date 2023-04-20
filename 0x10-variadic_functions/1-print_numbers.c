bbfc2882f2b1e4daaa48b6c75aeffff2ffc8f09e
Aya <aya_shamseldin@yahoo.com>
2023-04-20 14:32:52 -0700

    variadic functions project

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,14 +1,13 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- *
  * print_numbers - is a function that prints numbers
  *
  *@n: this is no of args
  *@...:to print the rest
- *@seperator: it is the string seperator
+ *@separator: it is the string seperator
  *
- * Return: return 
+ * Return: return
  *
  */
 
@@ -18,13 +17,15 @@ unsigned int i
 va_list args;
 va_start(args, n);
 
-	for (i= 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
+	printf("%d", args);
 	va_arg(args, int);
-	}
-	if (i < n - 1 && separator != NULL)
+
+	if (i < n - 1 && separator != NULL; i++)
 	{
 	printf("%s", separator);
+	}
 	}
 va_end(args);
 }
