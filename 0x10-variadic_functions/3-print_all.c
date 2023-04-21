bfb90808d386568825f433dabf9eece8e8a00eae
Aya <aya_shamseldin@yahoo.com>
2023-04-20 18:53:34 -0700

    var

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 #include "variadic_functions.h"
 
 /**
@@ -13,45 +14,71 @@
 void print_all(const char * const format, ...)
 {
 	va_list myarguments;
+	int i = 0, j = 0;
+	f_r forma[] = {
+		{'c', &print_char},
+		{'i', &print_integer},
+		{'f', &print_float},
+		{'s', &print_string},
+	};
+
 	va_start(myarguments, format);
-
-	char c;
-	int i;
-	float f;
-	char* s;
-	while (*format)
- 		{
-		if (*format == 'c')
-        	{
-            	c = (char)va_arg(myarguments, int);
-            	printf("%c", c);
-        	}
-        	else if (*format == 'i')
-        	{
-            	i = va_arg(myarguments, int);
-            	printf("%d", i);
-        	}
-        	else if (*format == 'f')
-        	{
-            	f = va_arg(myarguments, float);
-            	printf("%f", d);
-        	}
-        	else if (*format == 's')
-        	{
-            	s = va_arg(myarguments, char*);
-            	if (s == NULL)
-            	{
-                printf("(nil)");
-            	}
-            	else
-            	{
-                printf("%s", s);
-            	}
-        	}
-
-        	format++;
-    		}
+	while (format[i] != '\0')
+	{
+		while (j < 4)
+		{
+			if (forma[j].c == format[i])
+			{
+				forma[j].f(myarguments);
+				if (format[i] != format[strlen(format) - 1])
+					printf(", ");
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
 	va_end(myarguments);
-
 	printf("\n");
+}
+
+
+/**
+ * print_char - is a function that prints anything
+ *
+ * @argument: format
+ */
+void print_char(va_list argument)
+{
+	printf("%c", va_arg(argument, int));
+}
+
+/**
+ * print_string - is a function that prints anything
+ *
+ * @argument: format
+ */
+void print_string(va_list argument)
+{
+	printf("%s", va_arg(argument, char *));
+}
+
+/**
+ * print_integer - is a function that prints anything
+ *
+ * @argument: format
+ */
+void print_integer(va_list argument)
+{
+	printf("%d", va_arg(argument, int));
+}
+
+/**
+ * print_float - is a function that prints anything
+ *
+ * @argument: format
+ */
+void print_float(va_list argument)
+{
+	printf("%f", va_arg(argument, double));
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,18 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct formates - hold forms
+ * @c: arguments
+ * @f: hold function
+ */
+typedef struct formates
+{
+	char c;
+	void (*f)(va_list);
+} f_r;
+void print_char(va_list argument);
+void print_string(va_list argument);
+void print_float(va_list argument);
+void print_integer(va_list argument);
 #endif /* vardiac_functions_prototype_functions_H */
