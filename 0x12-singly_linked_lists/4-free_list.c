32b62df43c0ae80ff8abc1e6840c6e70c7d6013b
Aya <aya_shamseldin@yahoo.com>
2023-04-27 14:18:03 -0700

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - it will free the list
+ *
+ *@head: list to be free
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *te
+
+	while (head)
+	{
+		te = head;
+		head = head->next;
+		free(te->str);
+
+	}
+}
