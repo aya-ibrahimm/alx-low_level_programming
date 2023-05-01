46a93d34019141a12a6c14afc4841b73378aba20
Aya <aya_shamseldin@yahoo.com>
2023-05-01 13:47:43 -0700

    argv argc 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - func that multiplies two of the args
+ *
+ *@argc: args countr
+ *@argv: args that are there
+ *
+ * Return: always return 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
