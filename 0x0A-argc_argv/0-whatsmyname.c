6cd8548901a3830a4b58d0c98246c831925d8c90
Aya <aya_shamseldin@yahoo.com>
2023-05-01 11:52:39 -0700

    0 arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,15 @@
 /**
  * main - function taking args that'll print prog name
  *
- * Description : print prog name
+ * Description: print prog name
+ * @argc: counting args
+ * @argv: args
  *
- * Return : return 0 success
+ * Return: always return 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
-	printf("&s \n ", argv[0]);
+	printf("%s \n ", argv[0]);
 	return (0);
 }
