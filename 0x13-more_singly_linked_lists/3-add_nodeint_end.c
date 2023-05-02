efef5a307f343b682c68e896d97044192c191528
Aya <aya_shamseldin@yahoo.com>
2023-05-02 14:06:48 -0700

    3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_nodeint_end - print list and return number of elements
+ * @head: listint_t
+ * @n: n value for node
+ *
+ * Return: the number of elements
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *tmp = *head;
+
+	if (*head == NULL)
+	{
+		*head = malloc(sizeof(listint_t));
+		(*head)->n = n;
+		(*head)->next = NULL;
+		return (*head);
+	}
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = malloc(sizeof(listint_t));
+	if (tmp->next == NULL)
+	{
+		return (NULL);
+	}
+	(tmp->next)->n = n;
+	(tmp->next)->next = NULL;
+	return (tmp->next);
+}
