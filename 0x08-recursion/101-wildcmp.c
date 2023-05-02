7b83bc1739df3e282370f763e67db5cb7b82563e
Aya <aya_shamseldin@yahoo.com>
2023-05-02 09:17:27 -0700

    rec

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+/**
+  * wildcmp - gtg
+  * @s1: char
+  * @s2: char
+  * Return: 1 or 0
+  */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
